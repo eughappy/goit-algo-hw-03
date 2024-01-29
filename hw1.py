6b130ed73c26009ef4c8268adec529058c050990
eughappy <degai7454@gmail.com>
2024-01-29 15:19:42 +0200

    final

--- hw1.py
+++ hw1.py
@@ -3,7 +3,11 @@ from datetime import timedelta
 from datetime import datetime as dtdt
 def get_days_from_today(date):
     time_now = dtdt.now()
-    time_then = dtdt.strptime(date, '%Y-%m-%d')
+    try:
+        time_then = dtdt.strptime(date, '%Y-%m-%d')
+    except:
+        date = str(input("Введіть дату: "))
+        get_days_from_today(date)
     days_gone = (time_now - time_then)
     print(abs(days_gone.days))
 
